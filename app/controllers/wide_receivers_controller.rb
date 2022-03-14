cbfb1b95e4719a5390ca81d281307a328ebd89bf
DeeGeeWest <DeeGeeWest@gmail.com>
2022-03-14 15:54:03 -0400

    receivers api ready

--- app/controllers/wide_receivers_controller.rb
+++ app/controllers/wide_receivers_controller.rb
@@ -6,9 +6,19 @@ class WideReceiversController < ApplicationController
   end
   
   def show
-    user_input = params["depth"]
-    receiver = Receiver.find_by(id: user_input)
+    user_input = params["depth"].to_i
+    receiver = Receiver.find_by(depth: user_input)
     render json: receiver
+  end
+
+  def create
+    receiver = Receiver.new(
+      name: params["name"],
+      number: params["number"],
+      depth: params["depth"]
+    )
+    receiver.save
+    render json: receiver.as_json
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   get "/wide_receivers" => "wide_receivers#index"
   
-  get "/wide_receivers/depth" => "wide_receivers#show"
+  get "/wide_receivers/:depth" => "wide_receivers#show"
+
+  post "/wide_receivers/create" => "wide_receivers#create"
 
 end
